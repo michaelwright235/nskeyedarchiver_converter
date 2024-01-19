6d875602d6e9b86563a4efd8f0f28ac2ab7b136d
michaelwright235 <maiklpolkovnikov@gmail.com>
2024-01-19 15:48:53 +0300

    Rename structures & crate name

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ const VERSION_KEY_NAME: &str = "$version";
 const NULL_OBJECT_REFERENCE_NAME: &str = "$null";
 
 #[derive(Error, Debug)]
-pub enum NSKeyedUnarchiverError {
+pub enum ConverterError {
     #[error("Plist error: {0}")]
     PlistError(String),
     #[error("Expected '{0}' key to be a type of '{1}'")]
@@ -33,7 +33,7 @@ pub enum NSKeyedUnarchiverError {
     ExpectedUIDValue(String)
 }
 
-impl From<plist::Error> for NSKeyedUnarchiverError {
+impl From<plist::Error> for ConverterError {
     fn from(value: plist::Error) -> Self {
         Self::PlistError(
             match value.is_io() {
@@ -49,53 +49,53 @@ macro_rules! uid {
         match ($name.as_uid()) {
             Some(u) => u,
             None => {
-                return Err(NSKeyedUnarchiverError::ExpectedUIDValue($key))
+                return Err(ConverterError::ExpectedUIDValue($key))
             }
         }
     };
 }
 
-pub struct NSKeyedUnarchiver {
+pub struct Converter {
     objects: Vec<Value>,
     top: Dictionary,
 }
 
-impl NSKeyedUnarchiver {
-    pub fn new(plist: Value) -> Result<Self, NSKeyedUnarchiverError> {
+impl Converter {
+    pub fn new(plist: Value) -> Result<Self, ConverterError> {
         let Some(mut dict) = plist.into_dictionary() else {
-            return Err(NSKeyedUnarchiverError::WrongValueType("root", "Dictionary"));
+            return Err(ConverterError::WrongValueType("root", "Dictionary"));
         };
 
         // Check $archiver key
         let archiver_key = Self::get_header_key(&mut dict, ARCHIVER_KEY_NAME)?;
         let Some(archiver_str) = archiver_key.as_string() else {
-            return Err(NSKeyedUnarchiverError::WrongValueType(
+            return Err(ConverterError::WrongValueType(
                 ARCHIVER_KEY_NAME,
                 "String",
             ));
         };
 
         if archiver_str != ARCHIVER {
-            return Err(NSKeyedUnarchiverError::UnsupportedArchiver);
+            return Err(ConverterError::UnsupportedArchiver);
         }
 
         // Check $version key
         let version_key = Self::get_header_key(&mut dict, VERSION_KEY_NAME)?;
         let Some(version_num) = version_key.as_unsigned_integer() else {
-            return Err(NSKeyedUnarchiverError::WrongValueType(
+            return Err(ConverterError::WrongValueType(
                 VERSION_KEY_NAME,
                 "Number",
             ));
         };
 
         if version_num != ARCHIVER_VERSION {
-            return Err(NSKeyedUnarchiverError::UnsupportedArchiverVersion);
+            return Err(ConverterError::UnsupportedArchiverVersion);
         }
 
         // Check $top key
         let top_key = Self::get_header_key(&mut dict, TOP_KEY_NAME)?;
         let Some(top) = top_key.to_owned().into_dictionary() else {
-            return Err(NSKeyedUnarchiverError::WrongValueType(
+            return Err(ConverterError::WrongValueType(
                 TOP_KEY_NAME,
                 "Dictionary",
             ));
@@ -104,7 +104,7 @@ impl NSKeyedUnarchiver {
         // Check $objects key
         let objects_key = Self::get_header_key(&mut dict, OBJECTS_KEY_NAME)?;
         let Some(objects) = objects_key.into_array() else {
-            return Err(NSKeyedUnarchiverError::WrongValueType(
+            return Err(ConverterError::WrongValueType(
                 OBJECTS_KEY_NAME,
                 "Array",
             ));
@@ -115,30 +115,30 @@ impl NSKeyedUnarchiver {
             top,
         })
     }
-    pub fn from_file<P: AsRef<std::path::Path>>(path: P) -> Result<Self, NSKeyedUnarchiverError> {
+    pub fn from_file<P: AsRef<std::path::Path>>(path: P) -> Result<Self, ConverterError> {
         let val: plist::Value = plist::from_file(path)?;
         Self::new(val)
     }
 
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self, NSKeyedUnarchiverError> {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, ConverterError> {
         let val: plist::Value = plist::from_bytes(bytes)?;
         Self::new(val)
     }
 
     pub fn from_reader<R: std::io::Read + std::io::Seek>(
         reader: R,
-    ) -> Result<Self, NSKeyedUnarchiverError> {
+    ) -> Result<Self, ConverterError> {
         let val: plist::Value = plist::from_reader(reader)?;
         Self::new(val)
     }
 
-    pub fn decode(&mut self) -> Result<Value, NSKeyedUnarchiverError> {
+    pub fn decode(&mut self) -> Result<Value, ConverterError> {
         let mut dict = Dictionary::new();
         for (key, value) in &self.top {
             let uid = uid!(value, key.to_string());
             //println!("-- TOP: {key} (uid={}) --", uid.get());
             let Some(value) = self.decode_object(&uid.clone())? else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid.get()));
+                return Err(ConverterError::InvalidObjectEncoding(uid.get()));
             };
             dict.insert(
                 key.clone(),
@@ -148,14 +148,14 @@ impl NSKeyedUnarchiver {
         Ok(Value::Dictionary(dict))
     }
 
-    fn get_header_key(dict: &mut Dictionary, key: &'static str) -> Result<Value, NSKeyedUnarchiverError> {
+    fn get_header_key(dict: &mut Dictionary, key: &'static str) -> Result<Value, ConverterError> {
         let Some(objects_value) = dict.remove(key) else {
-            return Err(NSKeyedUnarchiverError::MissingHeaderKey(key));
+            return Err(ConverterError::MissingHeaderKey(key));
         };
         Ok(objects_value)
     }
 
-    fn decode_object(&self, uid: &plist::Uid) -> Result<Option<Value>, NSKeyedUnarchiverError> {
+    fn decode_object(&self, uid: &plist::Uid) -> Result<Option<Value>, ConverterError> {
         let object_ref = uid.get();
 
         if object_ref == 0 {
@@ -164,7 +164,7 @@ impl NSKeyedUnarchiver {
         }
 
         let Some(dereferenced_object) = self.objects.get(object_ref as usize) else {
-            return Err(NSKeyedUnarchiverError::InvalidObjectReference(object_ref));
+            return Err(ConverterError::InvalidObjectReference(object_ref));
         };
 
         if let Some(s) = dereferenced_object.as_string() {
@@ -177,14 +177,14 @@ impl NSKeyedUnarchiver {
         if Self::is_container(dereferenced_object) {
             //println!("decode_object: dereferenced_object (uid={object_ref}) is a container");
             let Some(dict) = dereferenced_object.as_dictionary() else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(object_ref));
+                return Err(ConverterError::InvalidObjectEncoding(object_ref));
             };
 
             let Some(class_reference_val) = dict.get("$class") else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(object_ref));
+                return Err(ConverterError::InvalidObjectEncoding(object_ref));
             };
             let Some(class_reference) = class_reference_val.as_uid() else {
-                return Err(NSKeyedUnarchiverError::InvalidClassReference(format!(
+                return Err(ConverterError::InvalidClassReference(format!(
                     "{:?}",
                     class_reference_val
                 )));
@@ -221,23 +221,23 @@ impl NSKeyedUnarchiver {
         }
     }
 
-    fn get_class_names(&self, uid: &plist::Uid) -> Result<Vec<&str>, NSKeyedUnarchiverError> {
+    fn get_class_names(&self, uid: &plist::Uid) -> Result<Vec<&str>, ConverterError> {
         //println!("get_class_names: uid = {}", uid.get());
 
         let Some(obj) = self.objects.get(uid.get() as usize) else {
-            return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid.get()));
+            return Err(ConverterError::InvalidObjectEncoding(uid.get()));
         };
 
         let Some(names) = obj
             .as_dictionary()
             .and_then(|dict| dict.get("$classes").and_then(|classes| classes.as_array())) else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid.get()));
+                return Err(ConverterError::InvalidObjectEncoding(uid.get()));
             };
 
         let mut vec_of_names = Vec::new();
         for name in names {
             let Some(name) = name.as_string() else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid.get()));
+                return Err(ConverterError::InvalidObjectEncoding(uid.get()));
             };
             vec_of_names.push(name);
         }
@@ -255,18 +255,18 @@ impl NSKeyedUnarchiver {
         }
     }
 
-    fn decode_custom_class(&self, uid: u64, val: &Dictionary) -> Result<Value, NSKeyedUnarchiverError> {
+    fn decode_custom_class(&self, uid: u64, val: &Dictionary) -> Result<Value, ConverterError> {
         let mut class_dict = Dictionary::new();
         for (key, value) in val {
             if key == "$class" {
                 //println!("{:?}", value);
                 let Some(classes_obj) = self.decode_object(uid!(value, key.to_string()))? else {
-                    return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+                    return Err(ConverterError::InvalidObjectEncoding(uid));
                 };
                 let Some(classes) = classes_obj
                     .as_dictionary()
                     .and_then(|dict| dict.get("$classes")) else {
-                        return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+                        return Err(ConverterError::InvalidObjectEncoding(uid));
                     };
                 class_dict.insert("$classes".to_string(), classes.clone());
                 continue;
@@ -285,10 +285,10 @@ impl NSKeyedUnarchiver {
         Ok(Value::Dictionary(class_dict))
     }
 
-    fn decode_array(&self, uid: u64, val: &Dictionary) -> Result<Value, NSKeyedUnarchiverError> {
+    fn decode_array(&self, uid: u64, val: &Dictionary) -> Result<Value, ConverterError> {
         //println!("decode_array: {:?}", val);
         let Some(raw_object) = val.get("NS.objects").and_then(|objs| objs.as_array()) else {
-            return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+            return Err(ConverterError::InvalidObjectEncoding(uid));
         };
         let mut array: Vec<Value> = Vec::with_capacity(raw_object.len());
         for element in raw_object {
@@ -302,12 +302,12 @@ impl NSKeyedUnarchiver {
         Ok(Value::Array(array))
     }
 
-    fn decode_dict(&self, uid: u64, val: &Dictionary) -> Result<Value, NSKeyedUnarchiverError> {
+    fn decode_dict(&self, uid: u64, val: &Dictionary) -> Result<Value, ConverterError> {
         let Some(keys) = val.get("NS.keys").and_then(|keys| keys.as_array()) else {
-            return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+            return Err(ConverterError::InvalidObjectEncoding(uid));
         };
         let Some(values) = val.get("NS.objects").and_then(|objs| objs.as_array()) else {
-            return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+            return Err(ConverterError::InvalidObjectEncoding(uid));
         };
         //println!("Decode dict, keys: {:?}", keys);
         //println!("Decode dict, values: {:?}", values);
@@ -317,13 +317,13 @@ impl NSKeyedUnarchiver {
         let mut decoded_values = Vec::with_capacity(values.len());
         for key in keys {
             let Some(decoded_key) = self.decode_object(uid!(key, "NS.keys".to_string()))? else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+                return Err(ConverterError::InvalidObjectEncoding(uid));
             };
             decoded_keys.push(decoded_key);
         }
         for value in values {
             let Some(decoded_value) = self.decode_object(uid!(value, "NS.objects".to_string()))? else {
-                return Err(NSKeyedUnarchiverError::InvalidObjectEncoding(uid));
+                return Err(ConverterError::InvalidObjectEncoding(uid));
             };
             decoded_values.push(decoded_value);
         }
