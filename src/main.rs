4c591d2d9e970bfb2d833829222a3876a5d73022
michaelwright235 <maiklpolkovnikov@gmail.com>
2024-01-19 21:06:04 +0300

    Add a json export option It uses serde_json under the hood

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use clap::Parser;
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+};
+
+use clap::{Args, Parser};
 use nskeyedarchiver_converter::{Converter, ConverterError};
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(author, version, about)]
-struct Args {
+struct Arguments {
     /// Path to a NSKeyedArchive encoded plist
-    file_in: String,
+    plist_in: String,
 
-    /// Path to an output plist
+    /// Path to an output file
     file_out: String,
 
-    /// Export plist in a binary format
-    #[arg(short)]
-    binary: bool,
+    #[command(flatten)]
+    output_format: Option<OutputFormat>,
 
     /// Leave $null values. By default they're omitted
     #[arg(short = 'n')]
@@ -21,19 +25,42 @@ struct Args {
     /// Treat dictionaries and arrays as regular classes. A $classes key gets retained.
     /// By default those are transformed into native plist structures.
     #[arg(short)]
-    treat_all_as_classes: bool
+    treat_all_as_classes: bool,
+}
+
+#[derive(Args, Clone)]
+#[group(required = false, multiple = false)]
+struct OutputFormat {
+    /// Export in a plist binary format
+    #[arg(short)]
+    binary: bool,
+
+    /// Export in a json format
+    #[arg(short)]
+    json: bool,
 }
 
 fn main() -> Result<(), ConverterError> {
-    let args = Args::parse();
-    let mut decoded_file = Converter::from_file(args.file_in)?;
+    let args = Arguments::parse();
+    let mut decoded_file = Converter::from_file(args.plist_in)?;
 
     decoded_file.set_leave_null_values(args.leave_null);
     decoded_file.set_treat_all_as_classes(args.treat_all_as_classes);
+    let decoded_value = decoded_file.decode()?;
 
-    match args.binary {
-        true => decoded_file.decode()?.to_file_binary(args.file_out)?,
-        false => decoded_file.decode()?.to_file_xml(args.file_out)?
+    if let Some(output_format) = args.output_format {
+        if output_format.binary {
+            decoded_value.to_file_binary(args.file_out)?
+        } else if output_format.json {
+            let json = serde_json::to_string(&decoded_value).unwrap();
+            let mut output = File::create(&args.file_out).unwrap();
+            let mut writer = BufWriter::new(&mut output);
+            writer.write_all(json.as_bytes()).unwrap();
+            return Ok(());
+        }
+    } else {
+        decoded_value.to_file_xml(args.file_out)?
     }
+
     Ok(())
 }
